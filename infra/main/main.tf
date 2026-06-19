45ea93cce7cc6417d466037a8ca25dc271f5ce8a
Nikolay Todorov <everflowing555@gmail.com>
2026-06-19 21:28:43 +0300

    feat: add Validate stage with terraform fmt/init/validate
    Fixes AB#31
    
    Fixes AB#32

--- infra/main/main.tf
+++ infra/main/main.tf
@@ -15,14 +15,14 @@ module "networking" {
 module "compute" {
   source = "../modules/compute"
 
-  prefix                = local.prefix
-  location              = var.location
-  resource_group_name   = azurerm_resource_group.rg_main.name
-  subnet_id             = module.networking.vmss_subnet_id
-  admin_username        = var.admin_username
-  admin_ssh_public_key  = var.admin_ssh_public_key
-  tags                  = local.common_tags
-  lb_backend_pool_id    = module.load_balancer.backend_pool_id
+  prefix               = local.prefix
+  location             = var.location
+  resource_group_name  = azurerm_resource_group.rg_main.name
+  subnet_id            = module.networking.vmss_subnet_id
+  admin_username       = var.admin_username
+  admin_ssh_public_key = var.admin_ssh_public_key
+  tags                 = local.common_tags
+  lb_backend_pool_id   = module.load_balancer.backend_pool_id
 }
 
 module "load_balancer" {
